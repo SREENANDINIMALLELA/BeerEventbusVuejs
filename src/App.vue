6629c8febacfb306418fafc2839535f8821e8cde
sreenandini <mallelasreenandini2@gmail.com>
2019-10-10 15:16:37 +0100

    added fav beer button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template lang="html">
+
   <div>
     <h1>Beers</h1>
     <div class="main-container">
       <beers-list :beers='beers'></beers-list>
       <beer-detail :beer='selectedbeer'></beer-detail>
+      <fav-beer :beers='favouriteBeer'></fav-beer>
     </div>
   </div>
 
@@ -13,6 +15,8 @@
 import {eventBus} from './main.js';
 import BeersList from './components/BeersList.vue';
 import BeerDetail from './components/BeerDetail.vue';
+import FavBeer from './components/FavBeer.vue';
+
 
 export default {
   name: 'app',
@@ -20,7 +24,7 @@ export default {
     return {
       beers: [],
       selectedbeer: null,
-      favouriteBeers: []
+      favouriteBeer: []
     };
   },
   mounted(){
@@ -31,14 +35,13 @@ export default {
       this.selectedbeer=beer;
     })
     eventBus.$on('fav-beers',(beer)=> {
-      debugger
-      console.log(beer);
-      this.favouriteBeers.push(beer)
+      this.favouriteBeer.push(beer)
     })
   },
   components: {
     "beers-list": BeersList,
-    "beer-detail": BeerDetail
+    "beer-detail": BeerDetail,
+    "fav-beer": FavBeer
     },
 
   }
---
+++ src/components/FavBeer.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div>
+    <h1>favouriteBeer</h1>
+    <ul>
+
+      <list-item v-for="(beer, index) in beers" :beer="beer" :key="index"></list-item>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+
+
+export default {
+  name: 'favourite-Beer',
+  props: ['beers'],
+
+  components: {
+    "list-item": ListItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
